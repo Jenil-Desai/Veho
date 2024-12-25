fcb80d05ace28c1d875c00d2bfdd5bde96f61941
Jenil Desai <jenildev91@gmail.com>
2024-12-26 00:38:10 +0530

    v1.3.1
    🧑🏻‍🔧 Security Fixes

--- configs/firebaseConfig.ts
+++ configs/firebaseConfig.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyASdpBJb0MO4C8jKqgt_O_7Sn6KTTezxvg",
-  authDomain: "veho-6f187.firebaseapp.com",
-  projectId: "veho-6f187",
-  storageBucket: "veho-6f187.firebasestorage.app",
-  messagingSenderId: "897977772467",
-  appId: "1:897977772467:web:1a0d0390414b7f26dd43c6",
-  measurementId: "G-C24NKX2C78",
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 export const app = initializeApp(firebaseConfig);
